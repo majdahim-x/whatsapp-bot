d6771269ce4da9b5ceb4e0ffaf5dfed6dd32e817
Mohammed Amine Ajdahim <amine.ajdahim.16@gmail.com>
2024-11-18 23:37:12 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import whatsappRoutes from './routes/whatsappRoutes';
 
 const app = express();
 app.use(express.json());
+app.use('/', (req, res) => {
+    res.send('Hello World');
+});
 app.use('/api', whatsappRoutes);
 
 export default app;
