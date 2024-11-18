52bd4f5dc22640acc16e2e1f1dbbd68d1dc23f8b
Mohammed Amine Ajdahim <amine.ajdahim.16@gmail.com>
2024-11-19 00:02:39 +0100

    add vercel.json

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import whatsappRoutes from './routes/whatsappRoutes';
 const app = express();
 app.use(express.json());
 
-app.use('/api', whatsappRoutes);
-//app get / route
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+app.use('/api', whatsappRoutes);
+//app get / route
 
 export default app;
